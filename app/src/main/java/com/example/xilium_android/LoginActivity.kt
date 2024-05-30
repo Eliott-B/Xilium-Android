7b51cc33ed14f45f7474eeb65037ce1ebc8933f6
Eliott-B <eliottb.info@gmail.com>
2024-05-30 11:39:49 +0200

    Ajout d'erreur et d'un utilisateur en dur

--- app/src/main/java/com/example/xilium_android/LoginActivity.kt
+++ app/src/main/java/com/example/xilium_android/LoginActivity.kt
@@ -7,6 +7,7 @@ import android.text.TextWatcher
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -47,8 +48,27 @@ class LoginActivity : AppCompatActivity() {
         val loginButton = findViewById<Button>(R.id.connect_ok)
 
         loginButton.setOnClickListener { v ->
+            login()
+        }
+    }
+
+    fun login() {
+        val emailEditText = findViewById<EditText>(R.id.editTextTextEmailAddress)
+        val passwordEditText = findViewById<EditText>(R.id.editTextTextPassword)
+
+        if (emailEditText.text.toString() == "paul.richar@gmail.fr" ||
+            passwordEditText.text.toString() == "azerty") {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+        else if (emailEditText.text.toString() == "" ||
+                    passwordEditText.text.toString() == "") {
+                val error = findViewById<TextView>(R.id.textView4)
+                error.text = getString(R.string.no_e_p)
+            }
+            else {
+                val error = findViewById<TextView>(R.id.textView4)
+                error.text = getString(R.string.bad_e_p)
+            }
     }
 }
