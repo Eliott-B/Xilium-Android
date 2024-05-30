13c54c036d32e5fe55b27bfff85ec666a2c9ef1b
victor g <20vg01@gmail.com>
2024-05-30 11:44:05 +0200

    Ajout du changement d'activité

--- app/src/main/java/com/example/xilium_android/MainActivity.kt
+++ app/src/main/java/com/example/xilium_android/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.xilium_android
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,20 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val btnConnect = findViewById<Button>(R.id.connect)
+        val btnTicket = findViewById<Button>(R.id.ticket)
+
+        btnConnect.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class)
+            startActivity(intent)
+        }
+
+        btnTicket.setOnClickListener {
+            val intent = Intent(this, TicketActivity::class)
+        }
+
     }
+
+
 }
